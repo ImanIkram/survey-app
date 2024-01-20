453679ed561a48607488b72e84d057513f11d825
iman <iman.ikram1122@gmail.com>
2024-01-19 20:26:08 -0500

    Student Survey server is complete

---
+++ server/config/dbConnection.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql'); // load the MySQL connection code which will communicate with the database.
+
+// create a configuration to access our database.
+//console.log(process.env.DB_PASSWORD)
+
+const dbUserConn = mysql.createPool({
+
+    connectionLimit: 10,
+
+    host: process.env.DB_HOST,
+
+    port: process.env.DB_PORT,
+
+    database: process.env.DB,
+
+    user: process.env.DB_USER,
+
+    password: process.env.DB_PASSWORD
+
+})
+
+// make the above code accessible for other code modules
+
+module.exports = dbUserConn;
